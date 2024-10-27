1059006dab9dbd9e662dbff407203b9c086c3918
bh-cell <bh626@scarletmail.rutgers.edu>
2024-10-27 12:57:51 -0400

    home background change

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to RuFriends</h1>
-            <Link to="/register">Register Here</Link>
-        </div>
-    );
+   return (
+       <div>
+            <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px' }}></nav>
+               <h1 style={{ color: 'rgb(204, 0, 51)' }}>Welcome to RU Friends</h1>
+               <Link
+               to="/register"
+                   style={{ fontFamily: 'Arial, sans-serif', color: 'red', textDecoration: 'none', margin: '0 10px' }}
+               >
+          
+               Register Here
+               </Link>
+       </div>
+   );
 };
+
 
 export default Home;
